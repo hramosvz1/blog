ee7e0886cfbbe63f814622e420aca44b29aa8856
hramosvz <tetor1900@gmail.com>
2017-12-21 09:17:27 -0600

    creacion de la base de datos

--- coreblog/admin.py
+++ coreblog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+# Re gister your models here.
+
+class UserMod(admin.ModelAdmin):
+	list_display = ["Username","First_N"]
+	list_display_links = ["Username"]
+	search_fields = ['User', 'First_N', 'Last_N']
+
+	 
+	class Meta:
+		model = User
+
+class PostMod(admin.ModelAdmin):
+	list_display = ["Title","Updated","Author"]
+	list_display_links = ["Title"]
+	search_fields = ['Title', 'Author', 'Category','Timestamp']
+	filter_horizontal = ('Category',)
+	 
+	class Meta:
+		model = Post
+class CategoryMod(admin.ModelAdmin):
+	list_display = ["Name",]
+	list_display_links = ["Name"]
+	search_fields = ['Name',]
+
+	 
+	class Meta:
+		model = Category
+
+admin.site.register(User, UserMod)
+admin.site.register(Post, PostMod)
+admin.site.register(Category, CategoryMod)
--- coreblog/models.py
+++ coreblog/models.py
@@ -1,6 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8    -*-
+import os, sys
 from django.db import models
 from django.utils import timezone
+import datetime
 # Create your models here.
 
 
 
+class User(models.Model):
+     Username= models.CharField(max_length=30)
+     First_N=models.CharField(max_length=50)
+     Last_N=models.CharField(max_length=50)
+     Password = models.CharField(max_length=30)
+
+     def __str__(self):
+     	return self.Username
+
+class Post(models.Model):
+     Title= models.CharField(max_length=100)
+     Content= models.TextField(max_length=3000)
+     Category= models.ManyToManyField('Category', blank=True) 
+     Author=models.ForeignKey('User',on_delete=models.CASCADE,)
+     Image=  models.ImageField(upload_to = 'head_img', default='')
+     Timestamp = models.DateTimeField(auto_now= False, auto_now_add=True)
+     Updated = models.DateTimeField(auto_now= True, auto_now_add=False)
+     
+     def __str__(self):
+        return self.Title
+
+class Category(models.Model):
+     Name= models.CharField(max_length=50)
+     
+     def __str__(self):
+        return self.Name
